986d5204efb13a690c4396b53ab49d2a69946db6
Bayu Stevanus Gerard Simamora <61579584+BayuBatam2008@users.noreply.github.com>
2024-05-05 18:17:18 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "Smithorigin",
+    template: "%s | Nungging",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "Om-om jualan kibod dan suka nungging",
   openGraph: {
-    title: "chronark.com",
+    title: "Smithorigin",
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+      "Om-om jualan kibod dan suka nungging",
+    url: "https://smithorigin.plj.my.id",
+    siteName: "Smithorigin",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://smithorigin.plj.my.id/og.png",
         width: 1920,
         height: 1080,
       },
@@ -36,10 +36,6 @@ export const metadata: Metadata = {
       "max-image-preview": "large",
       "max-snippet": -1,
     },
-  },
-  twitter: {
-    title: "Chronark",
-    card: "summary_large_image",
   },
   icons: {
     shortcut: "/favicon.png",
